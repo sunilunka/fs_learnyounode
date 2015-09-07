67a6f3cc2432faa34f9ecc8e67b485bf1a891309
Sunil Unka <sunil.unka@gmail.com>
2015-09-07 16:15:49 +1200

    Completed myFirstAsyncI_O task.

--- myFirstAsyncI_O.js
+++ myFirstAsyncI_O.js
@@ -1,1 +1,16 @@
 var fs = require("fs");
+
+var buffer = "";
+
+fs.readFile(process.argv[2], "utf-8", function(err, data){
+
+  if(err){
+    throw "An error occured: " + err + "\n Sorry about that!";
+  } else {
+    buffer = data;
+    var newLines = buffer.split("\n").length - 1 ;
+    console.log(newLines);
+  }
+
+});
+ 
