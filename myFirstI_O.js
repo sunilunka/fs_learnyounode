74f6470f99dd6c4667deea2a10c4b12a83060c17
Sunil Unka <sunil.unka@gmail.com>
2015-09-07 16:07:04 +1200

    Finished myFirstIO task.

--- myFirstI_O.js
+++ myFirstI_O.js
@@ -1,1 +1,6 @@
-var "fs" = require("fs");
+var fs = require("fs");
+
+var contentArray = fs.readFileSync(process.argv[2]).toString().split("\n");
+var newLines = contentArray.length - 1;
+
+console.log(newLines);
